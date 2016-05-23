bc844f03a2c0d71e0848e4f747da59ced1456c6b
unknown <kokowijanarko0228@gmail.com>
2016-05-23 11:00:42 +0700

    add database

---
+++ db/db_admin.sql
@@ -0,0 +1,190 @@
+/*
+SQLyog Ultimate v10.41 
+MySQL - 5.5.5-10.1.9-MariaDB : Database - db_admin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_admin` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_admin`;
+
+/*Table structure for table `admin_preferences` */
+
+DROP TABLE IF EXISTS `admin_preferences`;
+
+CREATE TABLE `admin_preferences` (
+  `id` tinyint(1) NOT NULL AUTO_INCREMENT,
+  `user_panel` tinyint(1) NOT NULL DEFAULT '0',
+  `sidebar_form` tinyint(1) NOT NULL DEFAULT '0',
+  `messages_menu` tinyint(1) NOT NULL DEFAULT '0',
+  `notifications_menu` tinyint(1) NOT NULL DEFAULT '0',
+  `tasks_menu` tinyint(1) NOT NULL DEFAULT '0',
+  `user_menu` tinyint(1) NOT NULL DEFAULT '1',
+  `ctrl_sidebar` tinyint(1) NOT NULL DEFAULT '0',
+  `transition_page` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `admin_preferences` */
+
+insert  into `admin_preferences`(`id`,`user_panel`,`sidebar_form`,`messages_menu`,`notifications_menu`,`tasks_menu`,`user_menu`,`ctrl_sidebar`,`transition_page`) values (1,0,0,0,0,0,1,0,0);
+
+/*Table structure for table `groups` */
+
+DROP TABLE IF EXISTS `groups`;
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `bgcolor` char(7) NOT NULL DEFAULT '#607D8B',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `groups` */
+
+insert  into `groups`(`id`,`name`,`description`,`bgcolor`) values (1,'admin','Administrator','#f44336'),(2,'members','General User','#2196F3');
+
+/*Table structure for table `inv_inventory` */
+
+DROP TABLE IF EXISTS `inv_inventory`;
+
+CREATE TABLE `inv_inventory` (
+  `inv_id` int(11) NOT NULL AUTO_INCREMENT,
+  `inv_name` varchar(100) DEFAULT NULL,
+  `inv_type_id` int(11) DEFAULT NULL,
+  `inv_category_id` int(11) DEFAULT NULL,
+  `inv_price` int(11) DEFAULT NULL,
+  `inv_stock` int(11) DEFAULT NULL,
+  `inv_desc` text,
+  PRIMARY KEY (`inv_id`),
+  KEY `inv_type_id` (`inv_type_id`),
+  KEY `inv_category_id` (`inv_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*Data for the table `inv_inventory` */
+
+insert  into `inv_inventory`(`inv_id`,`inv_name`,`inv_type_id`,`inv_category_id`,`inv_price`,`inv_stock`,`inv_desc`) values (2,'Kaos sablon warna',1,1,34000,123,'ceking adjah'),(3,'Kaos Sablon Emas dua',2,1,800000,23,'cek cek'),(4,'Mug Mug Mboh',3,2,456000,12,'mog coba'),(5,'Gantungan Kunci Pin A S U',4,3,65000,23,'adasdasd'),(6,'Kaos Pin A',4,1,3400000,12,'asdasdasd'),(7,'Mug Sablon Emas',2,2,45000000,450,'ceking'),(8,'Gantungan Kunci Pin A',4,3,65000,23,'adasdasd');
+
+/*Table structure for table `inv_ref_inventory_category` */
+
+DROP TABLE IF EXISTS `inv_ref_inventory_category`;
+
+CREATE TABLE `inv_ref_inventory_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) DEFAULT NULL,
+  `category_code` char(5) DEFAULT NULL,
+  `category_desc` text,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `inv_ref_inventory_category` */
+
+insert  into `inv_ref_inventory_category`(`category_id`,`category_name`,`category_code`,`category_desc`) values (1,'Kaos','K',NULL),(2,'Mug','Mg',NULL),(3,'Gantungan Kunci','GK',NULL),(4,'Pin','PN',NULL);
+
+/*Table structure for table `inv_ref_inventory_type` */
+
+DROP TABLE IF EXISTS `inv_ref_inventory_type`;
+
+CREATE TABLE `inv_ref_inventory_type` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(100) DEFAULT NULL,
+  `type_code` char(5) DEFAULT NULL,
+  `type_category_id` int(11) DEFAULT NULL,
+  `type_desc` text,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `inv_ref_inventory_type` */
+
+insert  into `inv_ref_inventory_type`(`type_id`,`type_name`,`type_code`,`type_category_id`,`type_desc`) values (1,'sablon warna','SW',1,NULL),(2,'Sablon Emas','SE',1,NULL),(3,'Mug Mboh','MB',2,NULL),(4,'Pin A','PNA',3,NULL),(5,'Pin B','PNB',3,NULL);
+
+/*Table structure for table `login_attempts` */
+
+DROP TABLE IF EXISTS `login_attempts`;
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(15) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `login_attempts` */
+
+/*Table structure for table `public_preferences` */
+
+DROP TABLE IF EXISTS `public_preferences`;
+
+CREATE TABLE `public_preferences` (
+  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `transition_page` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `public_preferences` */
+
+insert  into `public_preferences`(`id`,`transition_page`) values (1,0);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(15) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`ip_address`,`username`,`password`,`salt`,`email`,`activation_code`,`forgotten_password_code`,`forgotten_password_time`,`remember_code`,`created_on`,`last_login`,`active`,`first_name`,`last_name`,`company`,`phone`) values (1,'127.0.0.1','administrator','$2y$08$QOJBdCRUgZfdpv6L8uhzGuZas/PMDsFRU.2v8F7b6VWQ37azs7WSK','','admin@admin.com','',NULL,NULL,NULL,1268889823,1454147083,1,'Albertz','Chaniago','ADMIN','+62852 0000 0000');
+
+/*Table structure for table `users_groups` */
+
+DROP TABLE IF EXISTS `users_groups`;
+
+CREATE TABLE `users_groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`),
+  CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users_groups` */
+
+insert  into `users_groups`(`id`,`user_id`,`group_id`) values (2,1,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
